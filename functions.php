c048c329385fdc1de560e70c5e61dc1aba618d59
DenysHnatiuk <denys.hnatiuk@gmail.com>
2023-04-04 10:41:21 +0300

    add enqueue func

--- functions.php
+++ functions.php
@@ -1,4 +1,15 @@
 <?php
+/**
+ * Description:    main Pb Theme functions and definitions
+ *
+ * @package        snacks
+ * @since          1.0.0
+ * @author         Den Hnatiuk
+ * @copyright      Copyright © 2023, Den Hnatiuk(@denhnatiuk)
+ * @link           https://denyshnatiuk.github.io/Pb/
+ * @license        https://www.gnu.org/licenses/gpl-3.0.html GNU General Public License Version 3
+ */
+
 if ( ! function_exists( 'snacks_setup' ) ) :
 
 add_action( 'after_setup_theme', 'snacks_setup' );
@@ -9,6 +20,64 @@ function snacks_setup()
 
     add_editor_style( 'editor-style.css' );
 
+    /**
+    * // automaticly enabled features
+    * add_theme_support( 'post-thumbnails' );
+    * add_theme_support( 'responsive-embeds' );
+    * add_theme_support( 'editor-styles' );
+    * add_theme_support( 'html5', array('style','script', ) );
+    * add_theme_support( 'automatic-feed-links' );
+    */
+
+    // settings.typography.fontSizes
+    // add_theme_support( ‘editor-font-sizes’, array() );	 
+
+    // settings.typography.lineHeight
+    // add_theme_support( ‘custom-line-height’ ); 
+
+    //	settings.layout
+    // add_theme_support( ‘align-wide’ );
+    
+    // settings.color.palette
+    // add_theme_support( ‘editor-color-palette’, array() );
+    
+    // settings.color.gradients 
+    // add_theme_support( ‘editor-gradient-presets’, array() );	
+    
+    //	settings.spacing
+    // add_theme_support( ‘custom-spacing’ );
+    
+    //	settings.spacing.units
+    // add_theme_support( ‘custom-units’, array() );
+
 }
+
+add_action( 'wp_enqueue_scripts', 'snacks_assets' );
+function snacks_assets()
+{
+    
+	wp_enqueue_style( 'style', get_stylesheet_uri(), array(), '1' );
+
+	// wp_enqueue_style( 'slider', get_template_directory_uri() . '/css/slider.css', array(), '1.1', 'all' );
+
+	// wp_enqueue_script( 'script', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), 1.1, true );
+
+	// if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	// 	wp_enqueue_script( 'comment-reply' );
+	// }
+
+    // Block assets
+    // $styled_blocks = ['latest-comments'];
+	// foreach ( $styled_blocks as $block_name ) {
+	// 	$args = array(
+	// 		'handle' => "myfirsttheme-$block_name",
+	// 		'src'    => get_theme_file_uri( "assets/css/blocks/$block_name.css" ),
+	// 		$args['path'] = get_theme_file_path( "assets/css/blocks/$block_name.css" ),
+	// 	);
+	// 	wp_enqueue_block_style( "core/$block_name", $args );
+	// }
+}
+
+
 
 endif;
